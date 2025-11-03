be8918a1671e03a9b706d55535f702257f6a8d2a
Javeria Jalil <javeriajalil2@gmail.com>
2025-11-03 11:25:50 -0500

    getting started on assignment 5- Q2

---
+++ assignmnt5-Q2/app/src/main/java/com/example/assignment5_q2/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.assignment5_q2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.assignment5_q2.ui.theme.Assignment5_Q2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assignment5_Q2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+/**
+ * creating a sealed class to define the screeens in our app. this ensures type safe navigation and for keeping
+ * all screen related information (route,title,icon) in one place
+ */
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assignment5_Q2Theme {
+        Greeting("Android")
+    }
+}
