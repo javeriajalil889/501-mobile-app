600232e4ffbb0cb6155cc3b84a09e0023e38f378
Javeria Jalil <javeriajalil2@gmail.com>
2025-11-03 22:57:48 -0500

    starting Q3

---
+++ assignment5-Q3/app/src/main/java/com/example/q3_explore_bostons/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.q3_explore_bostons
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.q3_explore_bostons.ui.theme.Q3_Explore_BostonsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Q3_Explore_BostonsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
+    data object Home : Screen("home", "Home", Icons.Default.Home)
+    data object Categories : Screen("categories", "Categories", Icons.Default.Home)
+    data object List : Screen("list", "List", Icons.Default.Home)
+    data object Details : Screen("details", "Details", Icons.Default.Home)
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Q3_Explore_BostonsTheme {
+        Greeting("Android")
+    }
+}
