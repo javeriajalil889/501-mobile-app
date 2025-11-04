18ab5fc355d39504da65dd47145af29f6b9b919f
Javeria Jalil <javeriajalil2@gmail.com>
2025-11-04 12:24:23 -0500

    fixed gradle dependency issue, and NavGraph.kt clearly sets navigations routes for each screen

--- assignment5-Q3/app/src/main/java/com/example/q3_explore_bostons/NavGraph.kt
+++ assignment5-Q3/app/src/main/java/com/example/q3_explore_bostons/NavGraph.kt
@@ -1,27 +1,45 @@
 package com.example.q3_explore_bostons
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
-
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 @Composable
-fun NavGraph(navControlller: NavHostController){
+fun NavGraph(navController: NavHostController) {
     NavHost(
-        navController = navControlller,
+        navController = navController,
         startDestination = Screen.Home.route
-    ){
-        //Home Screen
-        composable(route = Screen.Home.route){
-            HomeScreen(navController = navControlller, modifier = Modifier)
+    ) {
+        // Home Screen
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController, modifier = Modifier)
         }
-        //Categories Screen
-        composable(route=Screen.Categories.route){
-            CategoriesScreen(navController = navControlller, modifier = Modifier)
+        // Categories Screen
+        composable(route = Screen.Categories.route) {
+            // FIX 2: And here
+            CategoriesScreen(navController = navController, modifier = Modifier)
         }
-        //List Screen
-
-        //Details Screen
-
+        // List Screen
+        composable(
+            route = "list/{category}",
+            arguments = listOf(navArgument("category") { type = NavType.StringType })
+        ) { backStackEntry ->
+            ListScreen(
+                // FIX 2: And here
+                navController = navController,
+                category = backStackEntry.arguments?.getString("category")
+            )
+        }
+        // Details Screen
+        composable(
+            route = "details/{placeId}",
+            arguments = listOf(navArgument("placeId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            DetailScreen(
+                placeId = backStackEntry.arguments?.getInt("placeId")
+            )
+        }
     }
 }
